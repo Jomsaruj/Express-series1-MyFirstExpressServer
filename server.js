ff39af8d396d912ee2752f1695a7f03edf9466bd
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2021-10-17 16:01:37 +0700

    handle post request

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(request,response){
 	response.send("<h1>This is root page</h1>");
@@ -15,6 +17,14 @@ app.get("/contact", function(request,response){
 
 app.get("/calculator", function(request,response){
 	response.sendFile(__dirname + "/index.html");
+})
+
+app.post("/calculator", function(request,response){
+	var num1 = Number(request.body.num1);
+	var num2 = Number(request.body.num2);
+	var result = num1+num2
+
+	response.send("Result = " + result)
 })
 
 app.listen(3000, function(){
