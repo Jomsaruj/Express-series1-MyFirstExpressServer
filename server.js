00bccf2a61af21a2086b2a97f5f332763dd2703d
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2021-10-17 15:34:12 +0700

    Add index.html and route /calculator

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
+
 app.get("/", function(request,response){
 	response.send("<h1>This is root page</h1>");
 })
+
 app.get("/about", function(request,response){
 	response.send("<h1>This is about page</h1>");
 })
+
 app.get("/contact", function(request,response){
 	response.send("<h1>This is contact page</h1>");
 })
+
+app.get("/calculator", function(request,response){
+	response.sendFile(__dirname + "/index.html");
+})
+
 app.listen(3000, function(){
 	console.log("Server started on port 3000");
 });
