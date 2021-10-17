dbc6739fb866f092b43990faceef857b99fcc9b8
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2021-10-17 14:03:08 +0700

    Add different routes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 app.get("/", function(request,response){
-	response.send("Hello");
+	response.send("<h1>This is root page</h1>");
+})
+app.get("/about", function(request,response){
+	response.send("<h1>This is about page</h1>");
+})
+app.get("/contact", function(request,response){
+	response.send("<h1>This is contact page</h1>");
 })
 app.listen(3000, function(){
 	console.log("Server started on port 3000");
